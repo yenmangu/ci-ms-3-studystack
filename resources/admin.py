c3699b6083f042d69460be69942704ee9357372d
Robert John Shelford <robert.shelford@googlemail.com>
2026-01-02 22:10:56 +0000

    feat(admin - actions): add mark all published action to admin; update ResourceAdmin to use Summernote

--- resources/admin.py
+++ resources/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Resource, Subject, SubjectResourceJoin
+from django_summernote.admin import SummernoteModelAdmin
+from .models import Resource, Subject, SubjectResourceJoin, Comment
 
 # Register your models here.
 
@@ -19,9 +20,27 @@ class SubjectResourceInline(admin.TabularInline):
     extra = 1
 
 
+@admin.action(description="Mark all as published")
+def mark_all_published(modeladmin, request, queryset):
+    queryset.update(status="p")
+
+
 @admin.register(Resource)
-class ResourceAdmin(admin.ModelAdmin):
+class ResourceAdmin(SummernoteModelAdmin):
+    """Customizes the admin interface for the Post model.
+    It uses Summernote for the 'content' field and provides
+    list display, search, filtering, and slug prepopulation.
+    """
+
     inlines = [SubjectResourceInline]
+    actions = [mark_all_published]
+
+    list_display = ("title", "slug", "status", "created_on", "author")
+    search_fields = ["title", "content"]
+    list_filter = ("status", "created_on")
+    prepopulated_fields = {"slug": ("title",)}
+    summernote_fields = ("content",)
 
 
 admin.site.register(Subject)
+admin.site.register(Comment)
